aba28988cde58f1c319da001ce55c2394c738155
Jesse <jessemckinney513@gmail.com>
2022-01-07 16:07:39 -0500

    yay

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,8 @@
 /*eslint-disable*/
 import React from "react";
 import { Container } from "reactstrap";
+import { Link } from "react-router-dom";
+import { Button } from "reactstrap";
 // used for making the prop types of this component
 import PropTypes from "prop-types";
 
@@ -9,51 +11,76 @@ function Footer(props) {
     <footer className={"footer" + (props.default ? " footer-default" : "")}>
       <Container fluid={props.fluid ? true : false}>
         <nav>
-          <ul>
-            <li>
-              <a
-                href="/"
-                target="_blank"
-              >
-                Jesse
-              </a>
-            </li>
-            <li>
-              <a
-                href="/"
-                target="_blank"
-              >
-                About Us
-              </a>
-            </li>
-            <li>
-              <a
-                href="/"
-                target="_blank"
-              >
-                Blog
-              </a>
-            </li>
-          </ul>
+          <div className="button-container">
+            <ul>
+              <li>
+                <Button
+                  className="btn-neutral btn-icon btn-round"
+                  color="default"
+                  href="https://www.facebook.com/MariettaVetClinic/"
+                  onClick={(e) => e.preventDefault()}
+                  size="lg"
+                >
+                  <i
+                    style={{ color: "steelblue", marginTop: "6px" }}
+                    className="fab fa-facebook-f fa-3x"
+                  />
+                </Button>
+              </li>
+              <li>
+                <Button
+                  className="btn-neutral btn-icon btn-round"
+                  color="default"
+                  href="https://www.linkedin.com/company/marietta-vet-clinic/"
+                  onClick={(e) => e.preventDefault()}
+                  size="lg"
+                >
+                  <i
+                    style={{ color: "royalblue", marginTop: "6px" }}
+                    className="fab fa-linkedin-in fa-3x"
+                  />
+                </Button>
+              </li>
+              <li>
+                <Button
+                  className="btn-neutral btn-icon btn-round"
+                  color="default"
+                  href="https://www.google.com/maps?cid=14756440109326445849"
+                  onClick={(e) => e.preventDefault()}
+                  size="lg"
+                >
+                  <i
+                    style={{
+                      background:
+                        "conic-gradient(from -45deg, #ea4335 110deg, #4285f4 90deg 180deg, #34a853 180deg 270deg, #fbbc05 270deg) 73% 55%/150% 150% no-repeat",
+                      WebkitBackgroundClip: "text",
+                      backgroundClip: "text",
+                      color: "transparent",
+                      WebkitTextFillColor: "transparent",
+                      marginTop: "6px",
+                    }}
+                    className="fab fa-google fa-3x"
+                  ></i>
+                </Button>
+              </li>
+            </ul>
+          </div>
+          {/*  */}
+          <br />
         </nav>
-        <div className="copyright">
-          &copy; {1900 + new Date().getYear()}, Designed by{" "}
+        <div style={{ marginTop: "4vh" }} className="copyright">
+          &copy; {1900 + new Date().getYear()},{" "}
           <a
-            href="/"
+            href="https://www.mariettavetclinic.com/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Jesse
+            Marietta Veterinary Clinic
+          </a>{" "}
+          &nbsp;{" "}
+          <a href="tel:4049195150" target="_blank" rel="noopener noreferrer">
+            (404)9195150
           </a>
-          . Coded by{" "}
-          <a
-            href="/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-          Jesse
-          </a>
-          .
         </div>
       </Container>
     </footer>
--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -3,9 +3,6 @@ import PerfectScrollbar from "perfect-scrollbar";
 import { Route, Switch, Redirect, useLocation } from "react-router-dom";
 import DemoNavbar from "components/Navbars/DemoNavbar.js";
 import Footer from "components/Footer/Footer.js";
-// import Sidebar from "components/Sidebar/Sidebar.js";
-// import FixedPlugin from "components/FixedPlugin/FixedPlugin.js";
-
 import routes from "routes.js";
 
 var ps;
@@ -38,7 +35,6 @@ function Admin(props) {
   };
   return (
     <div className="wrapper">
-      {/* <Sidebar {...props} routes={routes} backgroundColor={backgroundColor} /> */}
       <div className="main-panel" ref={mainPanel}>
         <DemoNavbar {...props} />
         <Switch>
@@ -55,10 +51,6 @@ function Admin(props) {
         </Switch>
         <Footer fluid />
       </div>
-      {/* <FixedPlugin
-        bgColor={backgroundColor}
-        handleColorClick={handleColorClick}
-      /> */}
     </div>
   );
 }
